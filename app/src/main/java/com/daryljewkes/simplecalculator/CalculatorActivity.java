8b5fc7eb89666db15a1539b06470ef6f912a9f2b
Daryl Jewkes <github@daryljewkes.com>
2014-09-08 17:51:20 +0100

    finished calculator

--- app/src/main/java/com/daryljewkes/simplecalculator/CalculatorActivity.java
+++ app/src/main/java/com/daryljewkes/simplecalculator/CalculatorActivity.java
@@ -1,17 +1,43 @@
 package com.daryljewkes.simplecalculator;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class CalculatorActivity extends Activity {
+public class CalculatorActivity extends Activity implements View.OnClickListener {
+
+    private EditText etNumber1, etNumber2;
+    private Button btnAdd, btnSubtract, btnMultiply, btnDivide;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        etNumber1 = (EditText) findViewById(R.id.etNumber1);
+        etNumber2 = (EditText) findViewById(R.id.etNumber2);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSubtract = (Button) findViewById(R.id.btnSubtract);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnDivide = (Button) findViewById(R.id.btnDivide);
+
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        btnAdd.setOnClickListener(this);
+        btnSubtract.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
+        btnDivide.setOnClickListener(this);
+
     }
 
 
@@ -32,5 +58,41 @@ public class CalculatorActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String operation = "";
+        float number1, number2, result = 0;
+
+        if (TextUtils.isEmpty(etNumber1.getText().toString()) || TextUtils.isEmpty(etNumber2.getText().toString())) {
+            return;
+        }
+
+        number1 = Float.parseFloat(etNumber1.getText().toString());
+        number2 = Float.parseFloat(etNumber2.getText().toString());
+
+        switch ( (view.getId())) {
+            case R.id.btnAdd:
+                operation = " + ";
+                result = number1 + number2;
+                break;
+            case R.id.btnSubtract:
+                operation = " - ";
+                result = number1 - number2;
+                break;
+            case R.id.btnMultiply:
+                operation = " x ";
+                result = number1 * number2;
+                break;
+            case R.id.btnDivide:
+                operation = " / ";
+                result = number1 / number2;
+                break;
+            default:
+                break;
+        }
+
+        tvResult.setText(number1 + operation + number2 + " = " + result);
     }
 }
